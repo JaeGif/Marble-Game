737d6100463500c1ff7e051e0a67986b31b435eb
Jae <giffordjacob0@gmail.com>
2024-08-09 13:40:11 -0700

    add wooden player skin

--- 66-create-a-game-with-r3f/src/Experience.jsx
+++ 66-create-a-game-with-r3f/src/Experience.jsx
@@ -1,16 +1,29 @@
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useTexture } from '@react-three/drei';
 import Lights from './Lights.jsx';
 import Level from './Level.jsx';
 import { Physics } from '@react-three/rapier';
 import Player from './Player.jsx';
+
 export default function Experience() {
+  const props = useTexture({
+    map: './textures/Poliigon_WoodVeneerOak_7760/1K/Poliigon_WoodVeneerOak_7760_BaseColor.jpg',
+    normalMap:
+      './textures/Poliigon_WoodVeneerOak_7760/1K/Poliigon_WoodVeneerOak_7760_Normal.jpg',
+    roughnessMap:
+      './textures/Poliigon_WoodVeneerOak_7760/1K/Poliigon_WoodVeneerOak_7760_ORM.jpg',
+    metalnessMap:
+      './textures/Poliigon_WoodVeneerOak_7760/1K/Poliigon_WoodVeneerOak_7760_ORM.jpg',
+    aoMap:
+      './textures/Poliigon_WoodVeneerOak_7760/1K/Poliigon_WoodVeneerOak_7760_ORM.jpg',
+  });
+
   return (
     <>
       <OrbitControls makeDefault />
       <Physics debug>
         <Lights />
         <Level />
-        <Player />
+        <Player {...props} />
       </Physics>
     </>
   );
--- 66-create-a-game-with-r3f/src/Player.jsx
+++ 66-create-a-game-with-r3f/src/Player.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { RigidBody } from '@react-three/rapier';
 
-function Player() {
+function Player(props) {
   return (
     <RigidBody
       canSleep={false}
       restitution={0.2}
       friction={1}
-      colliders='hull'
+      colliders='ball'
       position={[0, 1, 0]}
     >
       <mesh castShadow receiveShadow>
         <icosahedronGeometry args={[0.3, 1]} />
-        <meshStandardMaterial flatShading color={'mediumpurple'} />
+        <meshStandardMaterial
+          map={props.map}
+          metalnessMap={props.metalnessMap}
+          normalMap={props.normalMap}
+          roughnessMap={props.roughnessMap}
+          aoMap={props.aoMap}
+        />
       </mesh>
     </RigidBody>
   );
