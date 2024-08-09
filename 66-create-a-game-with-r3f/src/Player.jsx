1cb0ebe2900261ce6681885397a3f8d1ac91ca61
Jae <giffordjacob0@gmail.com>
2024-08-09 13:24:07 -0700

    adding player ball

---
+++ 66-create-a-game-with-r3f/src/Player.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { RigidBody } from '@react-three/rapier';
+
+function Player() {
+  return (
+    <RigidBody
+      restitution={0.2}
+      friction={1}
+      colliders='hull'
+      position={[0, 1, 0]}
+    >
+      <mesh castShadow receiveShadow>
+        <icosahedronGeometry args={[0.3, 1]} />
+        <meshStandardMaterial flatShading color={'mediumpurple'} />
+      </mesh>
+    </RigidBody>
+  );
+}
+
+export default Player;
