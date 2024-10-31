6767a6240183cd0d608f5eb62fdac8b71efa8a70
Jae <giffordjacob0@gmail.com>
2024-10-31 11:06:11 -0700

    put, del, get done

--- 66-create-a-game-with-r3f/src/hooks/useApi.jsx
+++ 66-create-a-game-with-r3f/src/hooks/useApi.jsx
@@ -37,14 +37,97 @@ function useApi() {
 
   function get(id, headers, options) {
     const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState(null);
+    const [err, setErr] = useState(null);
+
+    const sendGet = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(`${API_STRING}/scores/${id}`, {
+          method: 'GET',
+          body: JSON.stringify(jsonData),
+          headers: {
+            'Content-Type': 'application/json',
+            ...headers,
+          },
+          ...options,
+        });
+
+        const resData = await res.json();
+        setData(resData);
+      } catch (err) {
+        setErr(err);
+      }
+      setIsLoading(false);
+    };
+    useEffect(() => {
+      sendGet();
+    }, []);
+
+    return { data, isLoading, err };
   }
 
-  function put(id, headers, options) {
+  function put(jsonData, id, headers, options) {
     const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState(null);
+    const [err, setErr] = useState(null);
+
+    const sendPut = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(`${API_STRING}/scores/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify(jsonData),
+          headers: {
+            'Content-Type': 'application/json',
+            ...headers,
+          },
+          ...options,
+        });
+
+        const resData = await res.json();
+        setData(resData);
+      } catch (err) {
+        setErr(err);
+      }
+      setIsLoading(false);
+    };
+    useEffect(() => {
+      sendPut();
+    }, []);
+
+    return { data, isLoading, err };
   }
 
   function del(id, headers, options) {
     const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState(null);
+    const [err, setErr] = useState(null);
+
+    const sendDel = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(`${API_STRING}/scores/${id}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            ...headers,
+          },
+          ...options,
+        });
+
+        const resData = await res.json();
+        setData(resData);
+      } catch (err) {
+        setErr(err);
+      }
+      setIsLoading(false);
+    };
+    useEffect(() => {
+      sendDel();
+    }, []);
+
+    return { data, isLoading, err };
   }
 
   return [get, post, put, del];
