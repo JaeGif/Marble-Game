648bef8b028179c4da22147c6af06d22824b35a1
Jae <giffordjacob0@gmail.com>
2024-11-11 12:32:16 -0800

    reorg fixes phantom colliders

---
+++ 66-create-a-game-with-r3f/src/levels/Level1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Platform } from '../constructors/Blocks';
+
+function Level1() {
+  return (
+    <>
+      <Platform type={'start'} position={[0, 0, 0]} />
+    </>
+  );
+}
+
+export default Level1;
