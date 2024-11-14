e223edc2cd03845012c3deea949061a77a844198
Jae <giffordjacob0@gmail.com>
2024-11-14 11:02:07 -0800

    lvl 13 done

--- 66-create-a-game-with-r3f/src/levels/level13.jsx
+++ 66-create-a-game-with-r3f/src/levels/level13.jsx
@@ -39,7 +39,7 @@ function Level13() {
       <Platform
         type={'bounce'}
         position={[2, 0, 3]}
-        options={{ amplitude: 2, speed: 5, seed: Math.random() * 3 }}
+        options={{ amplitude: 0.1, speed: 5, seed: Math.random() * 3 }}
       />
       <Platform type={'spinner'} position={[3, 0, 3]} />
       <Platform
@@ -57,10 +57,11 @@ function Level13() {
       />
       <Platform type={'axe'} position={[-1, 0, 4]} />
       // empty
-      <Platform type={'speed'} position={[1, 0, 4]} />
+      <Platform type={'speed'} position={[1, 0, 4]} rotation={[0, 0, 0]} />
       <Platform
-        type={'floor'}
-        position={[1, -0.5, 4.5]}
+        type={'axe'}
+        options={{ floor: 'speed' }}
+        position={[1, -0.5, 4.45]}
         rotation={[Math.PI / 2, 0, 0]}
       />
       <Platform type={'floor'} position={[2, 0, 4]} />
@@ -91,14 +92,28 @@ function Level13() {
         position={[2, 0, 5]}
         rotation={[0, Math.PI / 2, 0]}
       />
-      // empty // Upper deck
+      // empty
+      <Platform
+        type={'axe'}
+        position={[0, 0, 7]}
+        rotation={[0, Math.PI / 2, 0]}
+      />
+      <Platform
+        type={'axe'}
+        options={{ floor: 'none' }}
+        position={[0, 0, 7]}
+        rotation={[0, Math.PI / 2, 0]}
+      />
+      <Platform type={'floor'} position={[2, 0, 7]} />
+      // Upper deck
       <Platform type={'blueHealth'} position={[1, -1, 5]} />
       <Platform
         type={'speed'}
         position={[0, -1, 5]}
-        rotation={[0, Math.PI / 2, 0]}
+        rotation={[0, -Math.PI / 2, 0]}
       />
       <Platform type={'limbo'} position={[0, -1, 6]} />
+      <Platform type={'spinner'} position={[0, -1, 7]} />
       // end
       <Platform type={'end'} position={[0, 0, 10]} />
     </>
