c3bfee3fe9fa33c7962f0bcdb5cef99a66e37974
Jae <giffordjacob0@gmail.com>
2024-11-18 10:47:44 -0800

    18 is 16 with lights

--- 66-create-a-game-with-r3f/src/levels/Level18.jsx
+++ 66-create-a-game-with-r3f/src/levels/Level18.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Platform } from '../constructors/Blocks';
 import FollowSpot from '../lights/FollowSpot';
 import Camera from '../interactables/Camera';
+import Lights from '../lights/Lights';
 
 function Level18() {
   return (
     <>
-      <color args={['#000']} attach='background' />
-      <FollowSpot />
-      <Camera modality='birdseye' />
+      <color args={['#bdedfc']} attach='background' />
+      <Camera debug modality='birdseye' />
+      <Lights />
       <Platform
         type={'start'}
         position={[0, 0, 0]}
