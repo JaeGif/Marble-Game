09a2ad3412bb73043f1b00340d56aade5860e9b8
Jae <giffordjacob0@gmail.com>
2024-11-18 12:54:46 -0800

    lvl set to 1

--- 66-create-a-game-with-r3f/src/stores/useGame.js
+++ 66-create-a-game-with-r3f/src/stores/useGame.js
@@ -8,7 +8,7 @@ export default create(
     return {
       obstacleCount: 8,
       jumps: 2,
-      level: 20,
+      level: 1,
       mode: 'casual',
       lives: [true, true, true],
       score: 0,
