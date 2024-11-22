4f24a7faf00bfd004e6d9e9a27fb52326b311471
Jae <giffordjacob0@gmail.com>
2024-11-21 23:30:05 -0800

    lvl 20 updated

--- 66-create-a-game-with-r3f/src/levels/Level20.jsx
+++ 66-create-a-game-with-r3f/src/levels/Level20.jsx
@@ -10,18 +10,51 @@ function Level20() {
       <Lights />
       <Camera modality='locked' />
       <Platform type={'start'} position={[0, 0, 0]} />
+      <Platform type={'floor'} position={[0, 0.5, 2]} />{' '}
+      <Platform type={'floor'} position={[0, 0.5, 1]} />
+      <Platform type={'floor'} position={[0, 0.5, 0]} />
+      <Platform type={'floor'} position={[0, 0.5, -1]} />
+      <Platform type={'floor'} position={[0, 0.5, -2]} />
+      <Platform
+        type={'speed'}
+        position={[0, 0, -2]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <Platform
+        type={'speed'}
+        position={[0, -1, -2]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <Platform
+        type={'gravity'}
+        position={[0, -5, -1]}
+        gravitationalConstant={7}
+        maxDistance={16}
+      />
+      <Platform
+        type={'gravity'}
+        position={[0, -5, 0]}
+        gravitationalConstant={6}
+        maxDistance={5}
+      />
+      <Platform type={'floor'} position={[0, 0, -1]} />
+      <Platform
+        type={'floor'}
+        position={[0, -0.5, -1.5]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <Platform type={'floor'} position={[0, 0.5, 0]} />
       <Platform
         type={'gravity'}
         position={[0, 0, 1]}
-        gravitationalConstant={2}
+        gravitationalConstant={8}
         maxDistance={8}
       />
-      <Platform type={'floor'} position={[0, 0, 2]} />
       <Platform
         type={'gravity'}
         position={[0, 0, 3]}
-        gravitationalConstant={-2}
-        maxDistance={8}
+        gravitationalConstant={-8}
+        maxDistance={5}
       />
       <Platform type={'floor'} position={[0, 0, 4]} />
       <Platform type={'end'} position={[0, 0, 5]} />
