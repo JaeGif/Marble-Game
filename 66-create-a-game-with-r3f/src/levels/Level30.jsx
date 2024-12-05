d5da0846f600653d1dd666bacbcb5f3c5c5df67e
Jae <giffordjacob0@gmail.com>
2024-12-05 11:41:31 -0800

    added third switch position

--- 66-create-a-game-with-r3f/src/levels/Level30.jsx
+++ 66-create-a-game-with-r3f/src/levels/Level30.jsx
@@ -28,7 +28,7 @@ function Level30() {
         type={'switch'}
         scale={{ x: 1, y: 3, z: 1 }}
         position={[
-          [-5, 1, 3],
+          [8, 0.1, 8],
           [0, 1.5, 5.55],
         ]}
         options={{ floor: 'floor', endGatePosition: [0, 1.3, 5.55] }}
@@ -46,7 +46,7 @@ function Level30() {
         type={'switch'}
         scale={{ x: 1, y: 3, z: 1 }}
         position={[
-          [-3, 1, 3],
+          [-7, 1.25, 3],
           [0, 1.5, 5.85],
         ]}
         options={{ floor: 'floor', endGatePosition: [0, 0.9, 6.15] }}
@@ -55,7 +55,7 @@ function Level30() {
         type={'switch'}
         scale={{ x: 1, y: 3, z: 1 }}
         position={[
-          [3, 1.15, 9],
+          [3, 2.05, 9],
           [0, 1.5, 6],
         ]}
         options={{ floor: 'floor', endGatePosition: [0, 0.7, 6.45] }}
@@ -84,6 +84,7 @@ function Level30() {
         options={{ floor: 'none' }}
       />
       <Platform type={'floor'} position={[4, 2, 9]} />
+      <Platform type={'floor'} position={[5, 2, 3]} />
       <Platform type={'axe'} position={[4, 2, 8]} />
       <Platform
         type={'speed'}
@@ -106,6 +107,88 @@ function Level30() {
         type={'passThrough'}
         position={[0.5, 2, 3]}
         scale={{ x: 6, y: 0.5, z: 0.5 }}
+      />
+      <Platform type={'floor'} position={[-3, 2, 3]} />
+      <Platform type={'floor'} position={[-4, 2, 3]} />
+      <Platform type={'floor'} position={[-5, 2, 3]} />
+      <Platform
+        type={'axe'}
+        position={[-5, 2, 2.8]}
+        rotation={[Math.PI / 2, 0, 0]}
+        options={{ floor: 'none' }}
+      />
+      <Platform
+        type={'axe'}
+        position={[-5, 1.8, 3.1]}
+        rotation={[Math.PI / 2, 0, 0]}
+        options={{ floor: 'none' }}
+      />
+      <Platform
+        type={'portal'}
+        position={[
+          [-8, 1.2, 3],
+          [0, -1, -1],
+        ]}
+        rotation={[
+          [0, Math.PI / 2, 0],
+          [0, 0, 0],
+        ]}
+      />
+      <Platform
+        type={'blueHealth'}
+        position={[-9, 1.2, 3]}
+        rotation={[0, Math.PI / 2, 0]}
+      />
+      <Platform type={'floor'} position={[-6, 1.3, 3]} />
+      <Platform
+        type={'portal'}
+        position={[
+          [0, 0, 3],
+          [7, 0, 3],
+        ]}
+        rotation={[
+          [0, 0, 0],
+          [0, -Math.PI / 2, 0],
+        ]}
+        options={{ floor: 'none' }}
+      />
+      <Platform type={'floor'} position={[7, 0, 3]} />
+      <Platform type={'floor'} position={[8, 0, 3]} />
+      <Platform type={'axe'} position={[8, 0, 4]} />
+      <Platform type={'floor'} position={[8, 0, 5]} />
+      <Platform type={'roundabout'} position={[8, 0, 6]} />
+      <Platform
+        type={'gravity'}
+        position={[8, 0, 6]}
+        gravitationalConstant={-5}
+        options={{ floor: 'none' }}
+      />
+      <Platform
+        type={'spinner'}
+        position={[8, 0, 6]}
+        scale={{ x: 2.25, y: 1, z: 1 }}
+        options={{ floor: 'none' }}
+      />
+      <Platform
+        type={'axe'}
+        position={[8, 0.2, 9]}
+        scale={{ x: 2, y: 2, z: 1 }}
+        rotation={[Math.PI / 2, 0, 0]}
+        options={{ floor: 'none' }}
+      />
+      <Platform
+        type={'axe'}
+        position={[7, 0.2, 10]}
+        scale={{ x: 2, y: 2, z: 1 }}
+        rotation={[Math.PI / 2, 0, 0]}
+        options={{ floor: 'none' }}
+      />
+      <Platform
+        type={'axe'}
+        position={[6, 0.2, 10]}
+        scale={{ x: 2, y: 2, z: 1 }}
+        rotation={[Math.PI / 2, 0, 0]}
+        options={{ floor: 'none' }}
       />
       <Platform type={'end'} position={[0, -0.2, 10]} />
     </>
