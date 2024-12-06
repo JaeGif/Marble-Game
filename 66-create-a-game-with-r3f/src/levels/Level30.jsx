23b64e378bb0eca8001ba1762cb5602e5e80c3a7
Jae <giffordjacob0@gmail.com>
2024-12-06 11:06:28 -0800

    lvl 30 done

--- 66-create-a-game-with-r3f/src/levels/Level30.jsx
+++ 66-create-a-game-with-r3f/src/levels/Level30.jsx
@@ -11,6 +11,31 @@ function Level30() {
       <Camera modality='locked' />
       <Platform type={'start'} position={[0, 0, 0]} />
       <Platform
+        type={'floor'}
+        position={[0.5, -0.15, 0]}
+        rotation={[0, 0, Math.PI / 2]}
+        scale={{ x: 0.3, y: 1, z: 1 }}
+      />
+      <Platform
+        type={'floor'}
+        position={[-0.5, -0.15, 0]}
+        rotation={[0, 0, Math.PI / 2]}
+        scale={{ x: 0.3, y: 1, z: 1 }}
+      />
+      <Platform
+        type={'floor'}
+        position={[0.5, -0.15, -1]}
+        rotation={[0, 0, Math.PI / 2]}
+        scale={{ x: 0.3, y: 1, z: 1 }}
+      />
+      <Platform
+        type={'floor'}
+        position={[-0.5, -0.15, -1]}
+        rotation={[0, 0, Math.PI / 2]}
+        scale={{ x: 0.3, y: 1, z: 1 }}
+      />
+      <Platform type={'floor'} position={[0, 0, -1]} />
+      <Platform
         type={'passThrough'}
         position={[0, 0.375, 2]}
         scale={{ x: 1.1, y: 0.75, z: 1.1 }}
@@ -32,15 +57,6 @@ function Level30() {
           [0, 1.5, 5.55],
         ]}
         options={{ floor: 'floor', endGatePosition: [0, 1.3, 5.55] }}
-      />
-      <Platform
-        type={'switch'}
-        scale={{ x: 1, y: 3, z: 1 }}
-        position={[
-          [-4, 1, 3],
-          [0, 1.5, 5.7],
-        ]}
-        options={{ floor: 'floor', endGatePosition: [0, 1.1, 5.85] }}
       />
       <Platform
         type={'switch'}
@@ -69,17 +85,30 @@ function Level30() {
         ]}
         rotation={[
           [Math.PI, 0, 0],
-          [0, 0, 0],
+          [Math.PI / 2, 0, 0],
         ]}
         options={{ floor: 'floor', endGatePosition: [0, 0.5, 6.75] }}
       />
       <Platform type={'flipGravity'} position={[-2, 0, 2]} />
+      <Platform type={'flipGravity'} position={[-2, -1.8, 4]} />
+      <Platform type={'floor'} position={[-2, -1, 4]} />
+      <Platform type={'floor'} position={[-2, -1.8, 3]} />
+      <Platform type={'floor'} position={[-2, -1.8, 2]} />
+      <Platform type={'floor'} position={[-2, -1.8, 1]} />
       <Platform
         type={'speed'}
         position={[-2, -1, 2]}
         rotation={[Math.PI, 0, 0]}
       />
-      <Platform type={'floor'} position={[-2, -1, 1]} />
+      <Platform
+        type={'switch'}
+        scale={{ x: 1, y: 3, z: 1 }}
+        position={[
+          [-2, -1.05, 1],
+          [0, 1.5, 5.7],
+        ]}
+        options={{ floor: 'floor', endGatePosition: [0, 1.1, 5.85] }}
+      />
       <Platform type={'floor'} position={[-2, -1, 0]} />
       <Platform
         type={'speed'}
@@ -90,6 +119,49 @@ function Level30() {
         type={'speed'}
         position={[-2, 0.5, -0.5]}
         rotation={[-Math.PI / 2, 0, 0]}
+      />{' '}
+      <Platform type={'floor'} position={[0, 0.05, -1]} />
+      <Platform type={'floor'} position={[0, 0.1, -1]} />
+      <Platform type={'flipGravity'} position={[0, 0.15, -1]} />
+      <Platform type={'floor'} position={[0, 1, -1]} />
+      <Platform type={'floor'} position={[0, 1, 0]} />
+      <Platform type={'floor'} position={[0, 1, 1]} />
+      <Platform type={'floor'} position={[0, 1, 2]} />
+      <Platform
+        type={'floor'}
+        position={[0.5, 0.85, 1]}
+        rotation={[0, 0, Math.PI / 2]}
+        scale={{ x: 0.3, y: 1, z: 1 }}
+      />
+      <Platform
+        type={'floor'}
+        position={[-0.5, 0.85, 1]}
+        rotation={[0, 0, Math.PI / 2]}
+        scale={{ x: 0.3, y: 1, z: 1 }}
+      />
+      <Platform
+        type={'floor'}
+        position={[0.5, 0.85, 0]}
+        rotation={[0, 0, Math.PI / 2]}
+        scale={{ x: 0.3, y: 1, z: 1 }}
+      />
+      <Platform
+        type={'floor'}
+        position={[-0.5, 0.85, 0]}
+        rotation={[0, 0, Math.PI / 2]}
+        scale={{ x: 0.3, y: 1, z: 1 }}
+      />
+      <Platform
+        type={'floor'}
+        position={[0.5, 0.85, -1]}
+        rotation={[0, 0, Math.PI / 2]}
+        scale={{ x: 0.3, y: 1, z: 1 }}
+      />
+      <Platform
+        type={'floor'}
+        position={[-0.5, 0.85, -1]}
+        rotation={[0, 0, Math.PI / 2]}
+        scale={{ x: 0.3, y: 1, z: 1 }}
       />
       <Platform
         type={'axe'}
