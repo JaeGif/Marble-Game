4e9aaeb6b2a8caa31015c2e1829c5cf7d4c760f8
Jae <giffordjacob0@gmail.com>
2025-01-01 11:48:53 -0800

    add ring effect

--- 66-create-a-game-with-r3f/src/constructors/shaders/flipGravity/ribbonFragment.glsl
+++ 66-create-a-game-with-r3f/src/constructors/shaders/flipGravity/ribbonFragment.glsl
@@ -1,4 +1,26 @@
+uniform sampler2D uTexture;
+uniform float uTime;
+varying vec2 vUv;
+
+
 void main() {
 
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+  // moving the ribbons up
+  vec2 ribbonUv = vUv;
+  ribbonUv.x *= 1.0;
+  ribbonUv.y *= 0.5;
+  ribbonUv.x -= uTime * 0.6;
+  // ribbonUv.y += uTime * 0.1;
+  float deadSpace = texture(uTexture, ribbonUv).r;
+  deadSpace = (1.0 - deadSpace);
+
+ // deadSpace *= smoothstep(0.0, 0.1, ribbonUv.x);
+ // deadSpace *= smoothstep(0.0, 0.1, 1.0 - ribbonUv.x);
+
+//  deadSpace *= smoothstep(0.0, 0.3, ribbonUv.y);
+//  deadSpace *= smoothstep(0.0, 0.7, 1.0 - ribbonUv.y);
+
+  gl_FragColor = vec4(1.0, 1.0, 1.0, deadSpace);
 }
+
